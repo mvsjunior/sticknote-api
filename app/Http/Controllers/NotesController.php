51a9a7c862ee55a4fded511df667438b8a289a3f
Mauro Vieira <mauro.emc@outlook.com>
2022-10-14 16:04:57 -0300

    #6 - Implementada a validação dos dados da requisição

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -3,12 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 
 class NotesController extends Controller
 {
-    //
-    public function create()
+
+    public function create(Request $request)
     {
-        return "It's work";
+        $execResult = [
+            "error" => false,
+            "message" => ""
+        ];
+
+        /*
+        | ---------------------------------------------------------------------
+        |    Validação da requisição
+        | ---------------------------------------------------------------------
+       */
+        $validationRules = [
+            "note" => "required|min:5|max:255"
+        ];
+
+        $validator = Validator::make($request->only('note'), $validationRules);
+
+        if($validator->fails())
+        {
+            $execResult['error']   = true;
+            $execResult['message'] = $validator->getMessageBag();
+        }
+
+        return json_encode($execResult);
     }
 }
