8f80de0ee2510ed3b08641a6d285b45ccdb27958
Mauro Vieira <mauro.emc@outlook.com>
2022-10-16 13:07:05 -0300

    #18 - Implementada a adição das colunas necessárias na tabela users, para usar a classe de testes do sanctum

---
+++ database/migrations/2022_10_16_160215_alter_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('token')->nullable();
+            $table->string('remember_token')->nullable();
+            $table->dateTime('email_verified_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('users', function (Blueprint $table)
+        {
+            $table->dropColumn('token');
+            $table->dropColumn('remember_token');
+            $table->dropColumn('email_verified_at');
+        });
+    }
+};
