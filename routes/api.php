8387c6fa9063b8cc570634387124ef4fd3a9553f
Mauro Vieira <mauro.emc@outlook.com>
2022-10-16 11:15:03 -0300

    #18 - Implementada uma camada de autenticação para o uso da rota de criação de notas

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::post('/user', [AuthController::class, 'create']);
 Route::post('/auth', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-Route::post('/note/create', [NotesController::class, 'create'])->name('note.create');
+Route::middleware('auth:sanctum')->post('/note/create', [NotesController::class, 'create'])->name('note.create');
 Route::get('/notes', [NotesController::class, 'getAll'])->name('note.getAll');
 Route::get('/note/{id}', [NotesController::class, 'select'])->name('note.select');
 Route::put('/note/update', [NotesController::class, 'update'])->name('note.update');
