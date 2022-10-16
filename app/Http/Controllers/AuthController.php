d4a3400a55fa71dcd3a2ffb235e02f2eca98f732
Mauro Vieira <mauro.emc@outlook.com>
2022-10-15 21:22:40 -0300

    #16 - Implementado método para criação de usuários

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Supporte\Facades\Auth;
+use Illuminate\Supporte\Facades\Hash;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     //
+    public function create(Request $request)
+    {
+        $array = ['error' => ''];
+
+        // validação
+        $rules = [
+            'email'    => 'required|email|unique:users,email',
+            'password' => 'required'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails())
+        {
+            $array['error'] = $validator->message();
+            return $array;
+        }
+
+        $email    = $request->input('email');
+        $password = $request->input('password');
+
+        // Criando novo usuário
+        $newUser           = new User();
+        $newUser->email    = $email;
+        $newUser->password = password_hash($password, PASSWORD_DEFAULT);
+        $newUser->token    = "";
+        $newUser->save();
+
+        // Logar o usuário recém criado.
+        return $array;
+    }
+
+
 }
