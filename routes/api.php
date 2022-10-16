f1ada66dc9e5e5f0f8e8ebdd77230298cdef4142
Mauro Vieira <mauro.emc@outlook.com>
2022-10-16 11:39:11 -0300

    #18 - Implementada uma camada de autenticação para o uso da rota para obtenção de uma nota específica (id)

--- routes/api.php
+++ routes/api.php
@@ -40,6 +40,6 @@ Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logo
 
 Route::middleware('auth:sanctum')->post('/note/create', [NotesController::class, 'create'])->name('note.create');
 Route::middleware('auth:sanctum')->get('/notes', [NotesController::class, 'getAll'])->name('note.getAll');
-Route::get('/note/{id}', [NotesController::class, 'select'])->name('note.select');
+Route::middleware('auth:sanctum')->get('/note/{id}', [NotesController::class, 'select'])->name('note.select');
 Route::put('/note/update', [NotesController::class, 'update'])->name('note.update');
 Route::delete('/note/delete', [NotesController::class, 'delete'])->name('note.delete');
