538061e280084d40f2e7583147ecdc67041b5385
Mauro Vieira <mauro.emc@outlook.com>
2022-10-16 13:13:41 -0300

    #18 - Aplicado ajuste no teste de seleção de uma nota específica, para interagir com a camada de autenticação

--- tests/Feature/NotesSelectTest.php
+++ tests/Feature/NotesSelectTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\Note;
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
 
 class NotesSelectTest extends TestCase
 {
@@ -15,14 +17,30 @@ class NotesSelectTest extends TestCase
      */
     public function test_route_select_all_notes_is_accessible()
     {
+        Sanctum::actingAs(
+            User::factory()->create(["token" => "fisdnfoisdoif"]),
+            ['*']
+        );
+
         $response = $this->get('/api/notes');
+
+        $userId = auth('sanctum')->user()->id;
+        User::destroy($userId);
 
         $response->assertStatus(200);
     }
 
     public function test_is_able_to_return_a_list_of_notes()
     {
+        Sanctum::actingAs(
+            User::factory()->create(["token" => "fisdnfoisdoif"]),
+            ['*']
+        );
+
         $response = $this->get('/api/notes');
+
+        $userId = auth('sanctum')->user()->id;
+        User::destroy($userId);
 
         $response->assertStatus(200)
                  ->assertJson([
@@ -33,15 +51,31 @@ class NotesSelectTest extends TestCase
 
     public function test_route_to_select_a_single_note_is_accessible()
     {
+        Sanctum::actingAs(
+            User::factory()->create(["token" => "fisdnfoisdoif"]),
+            ['*']
+        );
+
         $randomInt = random_int(1,999);
         $response = $this->get('/api/note/' . $randomInt);
+
+        $userId = auth('sanctum')->user()->id;
+        User::destroy($userId);
 
         $response->assertStatus(200);
     }
     
     public function test_is_able_to_return_a_single_note()
     {
+        Sanctum::actingAs(
+            User::factory()->create(["token" => "fisdnfoisdoif"]),
+            ['*']
+        );
+
         $response = $this->get('/api/note/999');
+
+        $userId = auth('sanctum')->user()->id;
+        User::destroy($userId);
 
         $response->assertStatus(200)
                  ->assertJson([
